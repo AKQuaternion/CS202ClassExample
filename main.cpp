6a2f59d4f255483f86e60111843426d35f13c696
Chris Hartman <chris.m.hartman@gmail.com>
2021-01-19 12:14:46 -0900

    Add function to display command line

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include "Foo.hpp"
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main(int argc, const char ** argv) {
-   std::cout << argc << " arguments, program name is " << argv[0] << "\n";
+using std::cout;
+
+void showCommandLine(std::vector<std::string> args) {
+   cout << "Your command line was:\n";
+   for (const auto s : args)
+      cout << s << " ";
+   cout << "\n";
+}
+int main(int argc, const char **argv) {
+   cout << argc << " arguments, program name is " << argv[0] << "\n";
+
+   std::vector<std::string> args;
+   for (int index = 0; index < argc; index++) {
+      args.push_back(argv[index]);
+   }
+
+   showCommandLine(args);
+
    Foo f;
    return 0;
 }
