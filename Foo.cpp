7c15cfe425ec0c1dd9087c97d934313693380701
Chris Hartman <chris.m.hartman@gmail.com>
2021-01-19 10:51:53 -0900

    Create Foo class

---
+++ Foo.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Chris Hartman on 1/19/21.
+//
+
+#include "Foo.hpp"
+#include<iostream>
+using std::cout;
+Foo::Foo() {
+   cout << "Called Foo default constructor.";
+}
---
+++ Foo.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Chris Hartman on 1/19/21.
+//
+
+#ifndef CS202CLASSEXAMPLE_FOO_HPP
+#define CS202CLASSEXAMPLE_FOO_HPP
+
+
+class Foo {
+   Foo();
+};
+
+
+#endif//CS202CLASSEXAMPLE_FOO_HPP
