1b55cb68d144d0e84e356a459cb3c327bd111969
Chris Hartman <chris.m.hartman@gmail.com>
2021-01-28 11:11:11 -0900

    Do some example code with unique and shared pointers.

--- main.cpp
+++ main.cpp
@@ -2,28 +2,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include "Pet.hpp"
+#include <memory>
+using std::unique_ptr;
+using std::make_unique;
 using std::cout;
-
+using std::move;
+using std::vector;
+using std::make_shared;
 
 int main() {
-  int x;
-  int y;
-  int *xPtr = &x;
-  x = 13;
-  cout << x << "\n";
-  cout << xPtr << "\n";
-  cout << *xPtr << "\n";
-  cout << &y << "\n";
-  --xPtr;
-  *xPtr = 14;
-  cout << y << "\n";
-  cout << &xPtr << "\n";
-  cout << sizeof(xPtr) << "\n";
-
-  int array[10] = {1,2,3,4};
-  int *aPtr = array;
-  ++aPtr;
-  cout << *aPtr << "\n";
-
+//   Pet dave("dave");
+//   unique_ptr<Pet> duPtr = make_unique<Pet>("uniqueBill");
+//   unique_ptr<Pet> p;
+//   p = move(duPtr);
+//   (*p).setName("will go away");
+//   p->setName("movedUniqueBill");
+//
+//   int x{6};
+//   Pet peter{"Peter"};
+//
+//   unique_ptr<Pet>(new Pet);
+   std::shared_ptr<Pet> second;
+   {
+      auto sPtr = make_shared<Pet>("sharedJenny");
+      second = sPtr;
+   }
+   cout << "I am here.\n";
+   second = make_shared<Pet>("Colleen");
 }
