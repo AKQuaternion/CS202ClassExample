767b116ae308e46bdb1ba68350fe3709e0c3716b
Chris Hartman <chris.m.hartman@gmail.com>
2021-02-04 10:11:44 -0900

    Sample code for appending

--- main.cpp
+++ main.cpp
@@ -12,31 +12,13 @@ using std::move;
 using std::string;
 using std::unique_ptr;
 using std::vector;
+using std::ostream;
+
+void output(ostream & os) {
+   os << "Hello world.\n";
+}
 
 int main() {
-   //   std::ifstream fudge("64317-0.txt");
-   //
-   //   std::string test;
-   //   while (std::getline(fudge ,test)) {
-   //      cout << test << " ";
-   //   }
-
-   while (true) {
-      cout << "Enter an int and a word: ";
-      string s;
-      getline(cin, s);
-      std::istringstream sin(s);
-      int i;
-      string word;
-      sin >> i >> word;
-      if (!sin) {
-         cout << "You messed up!\n";
-         if (sin.eof())
-            cout << "Because you didn't give enough input.\n";
-         sin.clear();
-      } else
-         cout << "int was " << i << " and word was " << word << std::endl;
-      if (i == 13)
-         break;
-   }
+      std::ofstream out("example.txt",std::ios::app);
+      out << "Hello world.\n";
 }
