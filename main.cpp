e7259d2306a501727b59c5e8d462c343c76a76b9
Chris Hartman <chris.m.hartman@gmail.com>
2021-02-11 10:18:29 -0900

    Overloaded operator << example

--- main.cpp
+++ main.cpp
@@ -1,2 +1,25 @@
-#define CATCH_CONFIG_MAIN // Tells catch to provide a main()
-#include "catch.hpp"
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+
+class Foo {
+   friend ostream & operator<<(ostream &os, const Foo &f);
+public:
+   Foo(int x) : _x(x) {}
+private:
+   int _x;
+};
+
+ostream & operator<<(ostream &os, const Foo &f) {
+   return os << "This is a Foo: " << f._x;
+}
+
+int main() {
+   Foo f(13);
+   cout << f;
+   return 0;
+}
+
+
+
