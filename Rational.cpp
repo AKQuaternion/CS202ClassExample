3793d318c4d0c5b8254fcaafd3089d5273211cc6
Chris Hartman <chris.m.hartman@gmail.com>
2021-02-18 10:29:08 -0900

    Write += and rewrite operator + in canonical form

--- Rational.cpp
+++ Rational.cpp
@@ -10,9 +10,11 @@ std::ostream &operator<<(std::ostream &os, const Rational &rhs) {
    return os << rhs._numerator << "/" << rhs._denominator;
 }
 
-Rational operator+(const Rational &lhs, const Rational &rhs) {
+Rational operator+(const Rational &lhs, const Rational &rhs) { //canonical
    //a/b + c/d = (ad+bc)/ad
-   return Rational(lhs._numerator * rhs._denominator + rhs._numerator * lhs._denominator, lhs._denominator * rhs._denominator);
+   auto temp{lhs};
+   temp += rhs;
+   return temp;
 }
 
 Rational & Rational::operator+=(const Rational &rhs) {
