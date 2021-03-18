cf6ce494ba9e809dfec49549d87763478403ee07
Chris Hartman <chris.m.hartman@gmail.com>
2021-03-18 09:04:14 -0800

    std::optional example

--- main.cpp
+++ main.cpp
@@ -1,2 +1,36 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp" //NOLINT
+#include <iostream>
+#include <optional>
+using std::cout;
+using std::cin;
+using std::optional;
+#include <string>
+using std::string;
+
+optional<int> getNumBooks() {
+   cout << "How many books would you like to buy? ";
+   int numBooks;
+   cin >> numBooks;
+   if (!cin) {
+      cin.clear();
+      cin.ignore(999,'\n');
+      return {};
+   }
+   if (numBooks<0)
+      return {};
+   return numBooks;
+}
+
+int main() {
+   int booksSoldSoFar = 0;
+   for(int i=0;i<10;++i) {
+      auto howMany = getNumBooks();
+      if (howMany)//can also use howMany.has_value()
+         cout << "The customer wanted " << howMany.value() << " books.\n";
+      else
+         cout << "The customer did not enter a valid value.\n";
+
+      booksSoldSoFar += howMany.value_or(0);
+      cout << booksSoldSoFar << " books have been sold.\n";
+   }
+   return 0;
+}
