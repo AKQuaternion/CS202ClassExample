67f151a937387a8f3f1f8071c5fae41adb515b81
Chris Hartman <chris.m.hartman@gmail.com>
2021-03-25 10:31:18 -0800

    Polymorphism Example

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using std::cin;
 using std::optional;
 #include <string>
 using std::string;
+#include <random>
 
 class Shape {
 public:
@@ -16,9 +17,8 @@ public:
    void setColor(const string &newColor) {
       _color = newColor;
    }
-   void describe() {
-      cout << "I am a base class Shape.\n";
-   }
+   virtual void describe() const =0;
+   virtual ~Shape() = default;
 private:
    string _color;
 };
@@ -35,30 +35,76 @@ public:
    void setRadius(double newRadius) {
       _radius = newRadius;
    }
-   void describe() {
+   virtual void describe() const override {
       cout << "I am a Circle.\n";
    }
 private:
    double _radius;
 };
 
+class Square : public Shape {
+public:
+   using Shape::Shape;
+
+   Square(double sideLength) : Shape("Turquoise"),_sideLength(sideLength)
+   {}
+   double getSideLength() const {
+      return _sideLength;
+   }
+   void setSideLength(double sideLength) {
+      _sideLength = sideLength;
+   }
+   virtual void describe() const override {
+      cout << "I am a Square.\n";
+   }
+private:
+   double _sideLength;
+};
+
+
+std::unique_ptr<Shape> getShape(char c) {
+   static std::random_device r;
+   static std::default_random_engine e1(r());
+   static std::uniform_int_distribution<int> flip(0, 1);
+
+   switch (c) {
+      case 's':
+      case 'S': // square
+         return std::make_unique<Square>("Red");
+      case 'c':
+      case 'C': // circle
+         return std::make_unique<Circle>("Red");
+      case 'r':
+      case 'R': // random
+         if (flip(e1) == 0)
+            return std::make_unique<Square>("Red");
+         else
+            return std::make_unique<Circle>("Red");
+      default:
+         throw std::runtime_error("Bad character parameter in getShape(c)");
+   }
+}
+
 int main() {
-   Shape s("Red");
-   cout << s.getColor() << "\n";
+//   Square s("Red");
+////   cout << s.getColor() << "\n";
+//
+//   s.setColor("Green");
+////   cout << s.getColor() << "\n";
+//
+//   Circle c(13.3);
+//   c.setRadius(5.4);
+//
+//   Circle anotherCircle("Pink");
+//
+//   c.setColor("Chartreuse");
+//
+////   cout << c.getColor() << "\n";
+//
+////   c.describe();
+////   s.describe();
 
-   s.setColor("Green");
-   cout << s.getColor() << "\n";
-
-   Circle c(13.3);
-   c.setRadius(5.4);
-
-   Circle anotherCircle("Pink");
-
-   c.setColor("Chartreuse");
-
-   cout << c.getColor() << "\n";
-
-   c.describe();
-   s.describe();
+   auto shapePtr = getShape('r');
+   shapePtr->describe();
    return 0;
 }
