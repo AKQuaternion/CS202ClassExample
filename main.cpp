bfbff0de7c05a0aa8aec04d9460e1a14d0b1e6bc
Chris Hartman <chris.m.hartman@gmail.com>
2021-04-08 10:34:56 -0800

    STL Examples

--- main.cpp
+++ main.cpp
@@ -9,17 +9,28 @@ using std::string;
 #include <vector>
 using std::vector;
 #include "MyPair.hpp"
+#include <algorithm>
+using std::begin;
+using std::end;
+using std::pair;
+
+void print(const vector<pair<int,char>>& v) {
+   for(auto [i,c]:v)
+      cout << "(" << i << "," << c << ") ";
+   cout << "\n";
+}
 
 int main() {
-   MyPair<int,char> m(2,'2');
-   cout << m.getFirst() << " " << m.getSecond() << "\n";
-   m.setFirst(4);
-   cout << m.getFirst() << " " << m.getSecond() << "\n";
+   vector<int> v{1,3,5,7,9,8,6,4,2};
+   cout << "The first even integer is " <<
+         *std::find_if(begin(v),end(v),[](int x){return x%2==0;});
 
-   auto m2 = makeMyPair(5,'%');
-   cout << m2.getFirst() << " " << m2.getSecond() << "\n";
-
-   cout << string(m2) << "\n";
-   cout << m2;
+   cout << "\n";
+   vector<pair<int,char>> vp{{1,'z'},{1,'a'},{3,'x'},{2,'y'}};
+   print(vp);
+   sort(begin(vp),end(vp));
+   print(vp);
+   sort(begin(vp),end(vp),[](auto a, auto b) {return a.second < b.second;});
+   print(vp);
    return 0;
 }
